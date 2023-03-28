c2206a2e00a474e7b44cb91e47245e3229ed954d
Alejandro Sanz Martin <alexsm32@gmail.com>
2023-03-28 13:35:36 +0300

    Added TotalWar

---
+++ Classes/Globals.lua
@@ -0,0 +1,13 @@
+-- SECTION: TotalWar
+Ala15vToolbox = {}
+Ala15vToolbox.TotalWar = {}
+Ala15vToolbox.TotalWar.StrategicZones = {}
+Ala15vToolbox.TotalWar.TotalWarDB = {}
+Ala15vToolbox.TotalWar.Main = false
+Ala15vToolbox.TotalWar.Methods = false
+Ala15vToolbox.TotalWar.Zones = false
+Ala15vToolbox.TotalWar.Brigades = false
+Ala15vToolbox.TotalWar.Airwings = false
+Ala15vToolbox.TotalWar.Fleets = false
+Ala15vToolbox.TotalWar.RandomReaction = false
+-- !SECTION
---
+++ Classes/TotalWar/Brigades.lua
@@ -0,0 +1,95 @@
+function TotalWar:onafterAddBrigades(From, Event, To)
+    local Settings = self.Settings
+    local BorderZones = self.Zones.BorderZones
+    local DBspZone = self.Zones.SpZones
+    local Coalition = Settings.TeamCoalition
+    local MainPrefix = Settings.MainPrefix
+
+    for _, Brigade in pairs(Settings.Brigades) do
+        --local brigadePrefix = MainPrefix .. " " .. Brigade.Prefix
+        local brigadePrefix = Brigade.Prefix
+        local brigadeAssets = Brigade.Assets
+
+        local statics = SET_STATIC:New()
+        --DBObject:FilterCoalitions(coalition) -- REVIEW: Should I add this?
+        statics:FilterPrefixes(brigadePrefix)
+        statics:FilterZones(BorderZones)
+        statics:FilterOnce()
+
+        for _, static in pairs(statics:GetSet()) do
+            local BrigadeName = static:GetName()
+            local Alias = CreateNameMGRS(Brigade.Prefix, static:GetCoordinate())
+
+            local SpawnZone
+            local SpawnDist = 1000
+            for _, zone in pairs(DBspZone:GetSet()) do
+                local dist = UTILS.VecDist2D(static:GetVec2(), zone:GetVec2())
+                if dist < SpawnDist then
+                    SpawnDist = dist
+                    SpawnZone = zone:GetName()
+                end
+            end
+
+            local Brigade = self:GenerateBrigade(BrigadeName, Alias, SpawnZone, Coalition)
+            if Brigade then
+                Brigade:SetMarker(true) --REVIEW
+
+                for _, asset in pairs(brigadeAssets) do
+                    local template = MainPrefix .. " " .. asset.Template
+                    local groups
+                    if asset.Economy then
+                        groups = self:GetWarehouseAssetProduction(Brigade:GetVec2(), asset.Economy.Factory,
+                            asset.Economy.Range, asset.Economy.Rate, Coalition)
+                    else
+                        groups = asset.Groups
+                    end
+                    local units = asset.Units
+                    local skill --REVIEW
+                    --local type = asset.Type
+                    local capabilities = asset.Capability
+
+                    if groups > 0 then
+                        local Platoon = self:GeneratePlatoon(template, groups, units, skill, Alias .. template,
+                        capabilities)
+                        if Platoon then
+                            Brigade:AddPlatoon(Platoon)
+                        end
+                    end
+                end
+
+                self.Chief:AddBrigade(Brigade)
+                --[[
+                -- SECTION: OnAfterAssetDead event
+                function Brigade:OnAfterAssetDead(From, Event, To, asset, request)
+                    local asset = asset --Functional.Warehouse#WAREHOUSE.Assetitem
+                    local request = request --Functional.Warehouse#WAREHOUSE.Pendingitem
+
+                    -- Get assignment.
+                    local assignment = Brigade:GetAssignment(request)
+
+                    -- Get nearest warehouse with the asset type
+                    local nearest, dist = Brigade:FindNeighborWarehouse(3, WAREHOUSE.Descriptor.ATTRIBUTE,
+                        asset.attribute,
+                        nil, nil, 3000)
+
+                    -- Resupply from the nearest warehouse
+                    if nearest then
+                        if asset.attribute == WAREHOUSE.Attribute.GROUND_INFANTRY then
+                            nearest:AddRequest(Brigade, WAREHOUSE.Descriptor.ATTRIBUTE, asset.attribute, 1,
+                                WAREHOUSE.TransportType.APC, nil, nil, "Resupply")
+                        else
+                            nearest:AddRequest(Brigade, WAREHOUSE.Descriptor.ATTRIBUTE, asset.attribute, 1, nil,
+                                nil,
+                                nil, "Resupply")
+                        end
+                    end
+                end
+                -- !SECTION
+                ]]
+                --
+            end
+        end
+    end
+end
+
+Ala15vToolbox.TotalWar.Brigades = true
